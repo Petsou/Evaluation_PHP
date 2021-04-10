f655866fd649ba252cafb7f18845e115ab4c6fa4
Phetsouvan <souv.phetsouvan@gmail.com>
2021-04-10 21:31:57 +0200

    Changement DB NAME & USER

--- config/global.php
+++ config/global.php
@@ -1,9 +1,9 @@
 <?php
 
 // DB_NAME correspond au nom de la base de données.
-define('DB_NAME', 'id16444954_immobilier');
+define('DB_NAME', 'id16444954_immobilier2');
 // DB_USER correspond au nom d'utilisateur de la base de données.
-define('DB_USER', 'id16444954_admin');
+define('DB_USER', 'id16444954_admin2');
 // DB_PASSWORD correspond au mot de passe de la base de données.
 define('DB_PASSWORD', 'Basededonnees!123');
 // DB_HOST renvoi l'hôte du serveur de base de données.
